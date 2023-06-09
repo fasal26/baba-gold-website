df3f73973b354c9dcf7829c1625bc8a6870cda58
fasal kp <fasalkp26@gmail.com>
2023-06-09 12:49:59 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // base: '/baba-gold-website/'
+  base: '/baba-gold-website/'
 })
